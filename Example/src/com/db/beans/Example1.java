76ab37d99d586783a850f97088de24a927ee00a0
Digvijaya Babu <chinnu7j@gmail.com>
2025-04-07 13:54:54 +0530

    hello

--- Example/src/com/db/beans/Example1.java
+++ Example/src/com/db/beans/Example1.java
@@ -1,7 +1,7 @@
 package com.db.beans;
 
 public class Example1 {
-	void m1()
+	public void m1()
 	{
 		System.out.println("hello");
 	}
---
+++ Example/src/com/db/test/Test.java
@@ -0,0 +1,12 @@
+package com.db.test;
+
+import com.db.beans.Example1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Example1 e1 = new Example1();
+		e1.m1();
+	}
+
+}
